4259abed7b5ce06e6c761cad175e9a121498d725
Rares Ioneac <81028729+raresioneac@users.noreply.github.com>
2021-10-27 17:29:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // Scenariu no input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔️ Introdu un numar!';
+    displayMessage('⛔️ Introdu un numar!');
+
+    // Scenariu castig
+  } else if (guess === secretNumber) {
+    // document.querySelector('.message').textContent = '🎉 Numarul e corect!';
+    displayMessage('🎉 Numarul e corect!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // Scenariu greseala
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > secretNumber ? '📈 Prea mare!' : '📉 Prea mic!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '💥 Ai pierdut!';
+      displayMessage('💥 Ai pierdut!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Incepe sa ghicesti!');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
